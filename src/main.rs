eac938005c78308f6238cc813d7b8577088cc6bb
Galedon <93209718+Galedon@users.noreply.github.com>
2026-07-20 16:49:54 +0200

    day 2 finished :)

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,42 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+    // get_day_1();
+    let mut n_valid = 0;
+    let data = read_file_to_string("input/2.txt")?;
+    for line in data.iter(){
+        let parts: Vec<_> = line.split(":").collect();
+        let policy = parts[0];
+        let password = parts[1];
+        let p_parts: Vec<_> = policy.split(" ").collect();
+        let p_char = p_parts[1];
+        let p_num = p_parts[0];
+        let p_num_parts: Vec<_> = p_num.split("-").collect();
+        let p_min: i32 = p_num_parts[0].parse().unwrap();
+        let p_max: i32 = p_num_parts[1].parse().unwrap();
+        let p_contain: i32 = password.chars()
+            .filter(|c| *c == p_char.chars().next().unwrap())
+            .count() as i32;
 
-    get_day_1();
-
-
+        if  (p_contain >= p_min)&&(p_contain <= p_max){n_valid += 1;}
+    }
+    println!("walid passwords: {}", n_valid);
     Ok(())
 }
 
 
-//noinspection RsUnresolvedMethod
-fn read_file(path: &str) -> Result<Vec<i32>, std::io::Error>{
+fn read_file_to_string(path: &str) -> Result<Vec<String>, std::io::Error>{
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut data = Vec::new();
+    for line_res in reader.lines() {
+        let line = line_res?;
+        data.push(line);
+    }
+    Ok(data)
+}
+
+fn read_file_to_int(path: &str) -> Result<Vec<i32>, std::io::Error>{
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     let mut data: Vec<i32> = Vec::new();
@@ -23,7 +49,7 @@ fn read_file(path: &str) -> Result<Vec<i32>, std::io::Error>{
 }
 
 fn get_day_1() ->  Result<(), Box<dyn std::error::Error>>{
-    let data = read_file("input/1.txt")?;
+    let data = read_file_to_int("input/1.txt")?;
     let mut a = 0;
     let mut b = 0;
     data.iter().for_each(|&x|
