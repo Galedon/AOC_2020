e6f824aee7b6fab7bdaae44dc219d36e7874ecb3
Galedon <93209718+Galedon@users.noreply.github.com>
2026-07-20 18:44:29 +0200

    3b complete :)

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         hill.push(row.chars().collect());
     }
 
-    let mut x = 0;
-    let mut y = 0;
-    let slope_x = 3;
-    let slope_y = 1;
+    let slope_vec = vec![
+        (1,1),
+        (3,1),
+        (5,1),
+        (7,1),
+        (1,2),
+        ];
+    let mut res:i64 = 1;
+    for slope in slope_vec {
+        let mut x = 0;
+        let mut y = 0;
+        let slope_x = slope.0;
+        let slope_y = slope.1;
+        // println!("{:?}", slope);
 
-    let x_base = hill[0].len();
-    let mut n_trees = 0;
-    while true{
-        if y >= hill.len(){break}
-        if hill[y][x%x_base] == '#'{n_trees += 1}
-        y += slope_y;
-        x += slope_x;
+        let x_base = hill[0].len();
+        let mut n_trees = 0;
+        loop {
+            if y >= hill.len() { break }
+            if hill[y][x % x_base] == '#' { n_trees += 1 }
+            y += slope_y;
+            x += slope_x;
+        }
+        res *= n_trees;
     }
-    println!("{}", n_trees);
+    println!("{}", res);
 
 
     Ok(())
@@ -40,63 +52,62 @@ fn read_file_to_string(path: &str) -> Result<Vec<String>, std::io::Error>{
     Ok(data)
 }
 
-fn read_file_to_int(path: &str) -> Result<Vec<i32>, std::io::Error>{
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let mut data: Vec<i32> = Vec::new();
-    for line_res in reader.lines() {
-        let line = line_res?;
-        data.push(line.parse().unwrap());
-    }
-    Ok(data)
-}
+// fn read_file_to_int(path: &str) -> Result<Vec<i32>, std::io::Error>{
+//     let file = File::open(path)?;
+//     let reader = BufReader::new(file);
+//     let mut data: Vec<i32> = Vec::new();
+//     for line_res in reader.lines() {
+//         let line = line_res?;
+//         data.push(line.parse().unwrap());
+//     }
+//     Ok(data)
+// }
 
-fn get_day_1() -> Result<(), Box<dyn std::error::Error>> {
-    let data = read_file_to_int("input/1.txt")?;
-    let mut a = 0;
-    let mut b = 0;
-
-    for &x in &data {
-        for &z in &data {
-            if x + z == 2020 {
-                a = x * z;
-            }
-            for &y in &data {
-                if x + y + z == 2020 {
-                    b = x * y * z;
-                }
-            }
-        }
-    }
-
-    println!("a = {}, b = {}", a, b);
-    Ok(())
-}
-fn get_day_2() -> Result<(), Box<dyn std::error::Error>> {
-    let mut n_valid_a = 0;
-    let mut n_valid_b = 0;
-    let data = read_file_to_string("input/2.txt")?;
-    for line in data.iter(){
-        let (policy, password) = line.split_once(':').ok_or("split")?;
-        let (range, letter) = policy.trim().split_once(' ').ok_or("split")?;
-        let (min_s, max_s) = range.split_once('-').ok_or("split")?;
-
-        let p1: usize = min_s.parse()?;
-        let p2: usize = max_s.parse()?;
-        let ch = letter.trim().chars().next().ok_or("char")?;
-
-        let count = password.chars().filter(|c| *c == ch).count();
-        if (p1..=p2).contains(&count) {
-            n_valid_a += 1;
-        }
-
-        if (password.chars().nth(p1) == Some(ch))^(password.chars().nth(p2) == Some(ch))
-        {
-            n_valid_b += 1;
-        }
-    }
-    println!("walid passwords a: {}", n_valid_a);
-    println!("walid passwords b: {}", n_valid_b);
-    Ok(())
-
-}
+// fn get_day_1() -> Result<(), Box<dyn std::error::Error>> {
+//     let data = read_file_to_int("input/1.txt")?;
+//     let mut a = 0;
+//     let mut b = 0;
+//
+//     for &x in &data {
+//         for &z in &data {
+//             if x + z == 2020 {
+//                 a = x * z;
+//             }
+//             for &y in &data {
+//                 if x + y + z == 2020 {
+//                     b = x * y * z;
+//                 }
+//             }
+//         }
+//     }
+//
+//     println!("a = {}, b = {}", a, b);
+//     Ok(())
+// }
+// fn get_day_2() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut n_valid_a = 0;
+//     let mut n_valid_b = 0;
+//     let data = read_file_to_string("input/2.txt")?;
+//     for line in data.iter(){
+//         let (policy, password) = line.split_once(':').ok_or("split")?;
+//         let (range, letter) = policy.trim().split_once(' ').ok_or("split")?;
+//         let (min_s, max_s) = range.split_once('-').ok_or("split")?;
+//
+//         let p1: usize = min_s.parse()?;
+//         let p2: usize = max_s.parse()?;
+//         let ch = letter.trim().chars().next().ok_or("char")?;
+//
+//         let count = password.chars().filter(|c| *c == ch).count();
+//         if (p1..=p2).contains(&count) {
+//             n_valid_a += 1;
+//         }
+//
+//         if (password.chars().nth(p1) == Some(ch))^(password.chars().nth(p2) == Some(ch))
+//         {
+//             n_valid_b += 1;
+//         }
+//     }
+//     println!("walid passwords a: {}", n_valid_a);
+//     println!("walid passwords b: {}", n_valid_b);
+//     Ok(())
+// }
