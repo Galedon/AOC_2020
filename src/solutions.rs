89a7b8899dde61d8431917ce9da7144cf97dafcb
Galedon <93209718+Galedon@users.noreply.github.com>
2026-07-21 22:50:08 +0200

    day 6 clean up

--- src/solutions.rs
+++ src/solutions.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
-
+use crate::solutions;
 
 pub fn read_lines_to_str(path: &str) -> io::Result<Vec<String>> {
     let file = File::open(path)?;
@@ -229,4 +229,36 @@ pub fn get_day_5()-> Result<String, Box<dyn std::error::Error>>{
     }
 
     Ok(format!("max id = {}, seat = {}", id_max, res_b ))
+}
+
+pub fn get_day_6()-> Result<String, Box<dyn std::error::Error>>{
+    let data = solutions::read_lines_to_str("input/6.txt")?;
+    let mut yes_sum = 0;
+    let mut yes_sum_b = 0;
+    let mut yes_set = HashSet::new();
+    let mut yes_set_b = HashSet::new();
+    let mut new_group = true;
+    for line in data{
+        if line.is_empty(){
+            yes_sum += yes_set.len();
+            yes_sum_b += yes_set_b.len();
+            yes_set.clear();
+            yes_set_b.clear();
+            new_group = true;
+            continue;
+        }
+
+        let mut yes_line = HashSet::new();
+        for ch in line.chars(){
+            yes_set.insert(ch);
+            yes_line.insert(ch);
+        }
+        if new_group{
+            yes_set_b = yes_line.clone();
+            new_group = false;
+            continue;
+        }
+        yes_set_b = yes_set_b.intersection(&yes_line).cloned().collect();
+    }
+    Ok(format!("res_a = {}, res_b = {}", yes_sum, yes_sum_b ))
 }
