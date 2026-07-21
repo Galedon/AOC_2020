5c5625bd0cc228f83cccef1f383c6b9f695abe67
Galedon <93209718+Galedon@users.noreply.github.com>
2026-07-21 22:46:43 +0200

    6b works :)

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,37 @@ use std::collections::HashSet;
 
 mod solutions;
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let data = solutions::read_lines_to_str("input/6.txt")?;
 
+    let data = solutions::read_lines_to_str("input/6.txt")?;
     let mut yes_sum = 0;
+    let mut yes_sum_b = 0;
     let mut yes_set = HashSet::new();
+    let mut yes_set_b = HashSet::new();
+    let mut new_group = true;
     for line in data{
         if line.is_empty(){
             yes_sum += yes_set.len();
+            yes_sum_b += yes_set_b.len();
             yes_set.clear();
+            yes_set_b.clear();
+            new_group = true;
             continue;
         }
+
+        let mut yes_line = HashSet::new();
         for ch in line.chars(){
             yes_set.insert(ch);
+            yes_line.insert(ch);
         }
+        if new_group{
+            yes_set_b = yes_line.clone();
+            new_group = false;
+            continue;
+        }
+        yes_set_b = yes_set_b.intersection(&yes_line).cloned().collect();
     }
     println!("{}", yes_sum);
-
-
+    println!("{}", yes_sum_b);
 
 
 
