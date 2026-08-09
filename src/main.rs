c7d237be392865729cb7106791fa2cb022bc0ad1
Galedon <93209718+Galedon@users.noreply.github.com>
2026-08-09 10:56:37 +0200

    day 9 clean up

--- src/main.rs
+++ src/main.rs
@@ -7,56 +7,6 @@ mod solutions;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let data = read_file_to_int_64("input/9.txt")?;
-    let mut the_number = 0;
-    let mut i = 26;
-
-    loop{
-        let mut ok = false;
-        for j in i-25 .. i {
-            for k in j+1 .. i {
-                if data[j]+data[k] == data[i] {
-                    ok = true;
-                }
-            }
-        }
-        // println!("i = {i}");
-        if !ok {
-            // println!("res_a = {}", data[i]);
-            the_number = data[i];
-            break}
-        i += 1
-    }
-    println!("The number is {}", the_number);
-
-    i = 0;
-    let mut j_final = 0;
-    loop {
-        let mut ok = false;
-        let mut sum = 0;
-
-        let n = data.len();
-        for j in 0 ..n {
-            sum += data[i+j];
-            if sum   == the_number{
-                ok = true;
-                j_final = j;
-                break;
-            }
-            if (sum > the_number) | (i + j > n)
-            {
-                break;
-            }
-        }
-
-        if ok {
-            break;
-        }
-        i += 1;
-    }
-    let  min = data[i..i+j_final].iter().min().unwrap();
-    let  max = data[i..i+j_final].iter().max().unwrap();
-
-    println!("{}", min + max);
 
 
     // println!("day 1:");
@@ -90,6 +40,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     // println!("day 8:");
     // println!("{}", solutions::get_day_8()?);
     // println!();
+
+    println!("day 9:");
+    println!("{}", solutions::get_day_9()?);
+    println!();
 
     Ok(())
 }
--- src/solutions.rs
+++ src/solutions.rs
@@ -406,3 +406,58 @@ pub fn get_day_8() -> Result<String, Box<dyn std::error::Error>>{
     }
     Ok(format!("res a = {}, res b = {}", res_a, res_b ))
 }
+
+pub fn get_day_9() -> Result<String, Box<dyn std::error::Error>>{
+    let data = read_file_to_int_64("input/9.txt")?;
+    let mut the_number = 0;
+    let mut i = 26;
+
+    loop{
+        let mut ok = false;
+        for j in i-25 .. i {
+            for k in j+1 .. i {
+                if data[j]+data[k] == data[i] {
+                    ok = true;
+                }
+            }
+        }
+        // println!("i = {i}");
+        if !ok {
+            // println!("res_a = {}", data[i]);
+            the_number = data[i];
+            break}
+        i += 1
+    }
+    // println!("The number is {}", the_number);
+
+    i = 0;
+    let mut j_final = 0;
+    loop {
+        let mut ok = false;
+        let mut sum = 0;
+
+        let n = data.len();
+        for j in 0 ..n {
+            sum += data[i+j];
+            if sum   == the_number{
+                ok = true;
+                j_final = j;
+                break;
+            }
+            if (sum > the_number) | (i + j > n)
+            {
+                break;
+            }
+        }
+
+        if ok {
+            break;
+        }
+        i += 1;
+    }
+    let  min = data[i..i+j_final].iter().min().unwrap();
+    let  max = data[i..i+j_final].iter().max().unwrap();
+
+    // println!("{}", min + max);
+    Ok(format!("res_a = {}, res_b = {}", the_number, min + max))
+}
